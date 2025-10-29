103c347c0f11122fe3e42265438074c92f1e5525
Idobenhamo <idobenhamo@users.noreply.github.com>
2025-10-29 12:43:34 +0200

    Refactor helix and wezterm configurations for improved clarity and organization

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,21 @@
 local wezterm = require 'wezterm'
-local config = {}
+local config = wezterm.config_builder()
 
--- Doom Emacs color theme
-config.color_scheme = 'DoomOne'
+-- General
+config.font_size = 12
+config.line_height = 1.2
+config.color_scheme = 'DoomOne'-- Doom Emacs color theme
+config.default_cursor_style = 'SteadyBar' -- Cursor style
 
--- Hide the title bar
-config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false -- Hide the title bar
+-- config.hide_tab_bar_if_only_one_tab = true
 
 -- Fonts configuration
-config.font = wezterm.font_with_fallback { 'JetBrains Mono Nerd Font', 'DejaVu Sans'}
-
--- Cursor style
-config.default_cursor_style = 'SteadyBar'
-
--- Bidi support
-config.bidi_enabled = true
+config.font = wezterm.font_with_fallback{
+  'JetBrains Mono Nerd Font',
+  'DejaVu Sans'
+}
+config.bidi_enabled = true -- BIDI support
 
 return config
